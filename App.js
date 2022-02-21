23034c4dc085315b2337e3aa882a646ccb5b940a
sung uk <sunguk0204@monkey-soft.com>
2022-02-21 13:44:08 +0900

    app.js 12 error

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const name = 'sunguk'; 
+  return ( 
+    <View style={styles.container}> 
+      <Text style={styles.text}>My name is {name}</Text>
+      <StatusBar Style="auto" />
+    </View>>
+    {});
+  }
+
 
 const styles = StyleSheet.create({
   container: {
